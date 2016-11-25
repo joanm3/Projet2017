89960f35614eaa10daaf2ca210a1675a5cc80f13
Farrand <rf.robert@yahoo.fr>
2016-11-25 10:43:46 +0100

    GAME WITH new controller

---
+++ Assets/UpdateCollider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateCollider : MonoBehaviour {
+
+	SkinnedMeshRenderer myMeshRenderer;
+	MeshCollider myCollider;
+
+	void UpdateColliderMesh() {
+		Mesh colliderMesh = new Mesh();
+		myMeshRenderer.BakeMesh(colliderMesh);
+		myCollider.sharedMesh = null;
+		myCollider.sharedMesh = colliderMesh;
+	}
+
+	void Start(){
+		myCollider = GetComponent<MeshCollider>();
+		myMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+	}
+
+	void Update(){
+
+		UpdateColliderMesh();
+
+	}
+}
