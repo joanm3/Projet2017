2401f2aa61d015b23bc28cf8dd9d45bb86bc6da0
joanm3 <joanm3@hotmail.com>
2016-11-28 20:02:37 +0100

    [Surfaces] Added surfaceManager and SurfaceConfig. Adding Singleton Behaviour for SurfaceConfig (still with errors).

---
+++ Assets/Proto 5 Giant mecanim scriptable/Scripts/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Be aware this will not prevent a non singleton constructor
+///   such as `T myT = new T();`
+/// To prevent that, add `protected T () {}` to your singleton class.
+/// 
+/// As a note, this is made as MonoBehaviour because we need Coroutines.
+/// </summary>
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	public static T Instance; 
+
+	private void Awake()
+	{
+		if (Instance == null) 
+		{
+			Instance = T; 
+		} 
+		else if (Instance != this) 
+		{
+			Destroy (this); 
+		}
+
+		OnAwake (); 
+
+	}
+
+	internal void OnAwake()
+	{
+	}
+
+
+	private void OnDestroy()
+	{
+		Instance = null; 
+	}
+
+}
