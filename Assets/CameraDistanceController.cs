5a478491a39e84ae9ee26fa9edf8b36aa0a4a531
joanm3 <joanm3@hotmail.com>
2016-11-30 13:03:01 +0100

    [Camera] Added fadeOut fadeIn with all collisions for not viewing the ending of the object. Still to improve // Added a distance controller for camera that changes the camera distance when  entering the trigger.

---
+++ Assets/CameraDistanceController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDistanceController : MonoBehaviour
+{
+	public ThirdPersonCameraMovement thirdPersonCameraMovement;
+
+	public float newDistance = 10f;
+
+	[Range(0,1)]
+	public float lerpTime = 0.1f;
+
+	private bool changeDistance = false;
+
+	private float startTime; 
+	private float journeyLength; 
+
+	void Start ()
+	{
+		startTime = Time.time; 
+		if (thirdPersonCameraMovement == null)
+			thirdPersonCameraMovement = Camera.main.GetComponentInParent<ThirdPersonCameraMovement> (); 
+		
+		journeyLength = Mathf.Abs (thirdPersonCameraMovement.distance - newDistance); 
+
+	}
+
+	void Update ()
+	{
+		if (changeDistance == true) {
+
+			float distCovered = (Time.time - startTime) * lerpTime; 
+			float fracJourney = distCovered / journeyLength; 
+
+			thirdPersonCameraMovement.distance = Mathf.Lerp (thirdPersonCameraMovement.distance, newDistance, fracJourney); 
+
+			if (Mathf.Abs (thirdPersonCameraMovement.distance - newDistance) >= 0.1f) {
+				thirdPersonCameraMovement.distance = newDistance;
+				changeDistance = false; 
+			}
+		}
+
+
+	}
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("trigger entered"); 
+		Debug.Log (other.name); 
+		if (other.tag == "Player") {
+			startTime = Time.time; 
+			journeyLength = Mathf.Abs (thirdPersonCameraMovement.distance - newDistance); 
+			changeDistance = true; 
+			Debug.Log ("player entered"); 
+		}
+	}
+
+
+
+		
+
+
+
+}
--- Assets/CameraTest/ThirdPersonCameraMovement.cs
+++ Assets/CameraTest/ThirdPersonCameraMovement.cs
@@ -5,6 +5,7 @@ public class ThirdPersonCameraMovement : MonoBehaviour
 {
 
 	public Transform playerTransform;
+	public CharacterController playerController; 
 
 	public float distance = 10f;
 	public float currentX = 0.0f;
@@ -15,7 +16,6 @@ public class ThirdPersonCameraMovement : MonoBehaviour
 	public LayerMask obstacleMask;
 	public LayerMask fadeMask;
 
-
 	private Transform m_transform;
 	private Camera m_cam;
 	private float m_trueDistance;
@@ -23,8 +23,8 @@ public class ThirdPersonCameraMovement : MonoBehaviour
 	private const float Y_ANGLE_MIN = -20.0f;
 	private const float Y_ANGLE_MAX = 50.0f;
 
-	private static string obstacleLayerString = "Obstacle"; 
-	private static string fadeleLayerString = "Fade"; 
+	private static string obstacleLayerString = "Obstacle";
+	private static string fadeleLayerString = "Fade";
 
 
 	// Use this for initialization
@@ -45,7 +45,7 @@ public class ThirdPersonCameraMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-
+		
 
 
 
@@ -56,8 +56,9 @@ public class ThirdPersonCameraMovement : MonoBehaviour
 
 	}
 
-	private bool m_alphaChanged = false; 
-	public MeshRenderer m_alphaFadeRenderer = null; 
+	private bool m_fadeRaycastEntered = false;
+	private bool m_obstacleRaycastEntered = false;
+	public MeshRenderer m_colliderRenderer = null;
 
 	void FixedUpdate ()
 	{
@@ -65,7 +66,7 @@ public class ThirdPersonCameraMovement : MonoBehaviour
 		RaycastHit _hit; 
 		
 		Debug.DrawRay (playerTransform.position, -_rayDirection, Color.red); 
-		Material _hitMaterial;  
+		//Material _hitMaterial;  
 		//better if made in one single raycast, not two. to see later on. 
 		if (Physics.Raycast (playerTransform.position, -_rayDirection, out _hit, distance, SumLayers (obstacleMask, fadeMask))) {
 //			Debug.Log (LayerMask.NameToLayer("Obstacle")); 
@@ -74,16 +75,26 @@ public class ThirdPersonCameraMovement : MonoBehaviour
 
 
 			if (_hit.collider.gameObject.layer == LayerMask.NameToLayer (obstacleLayerString)) {
-				m_trueDistance = Mathf.Lerp (m_trueDistance, _hit.distance, Time.fixedDeltaTime * lerpVelocity * 3f);
+				
+				m_colliderRenderer = _hit.collider.GetComponent<MeshRenderer> (); 
+				//m_colliderRenderer.enabled = false; 
+				float _lerpedAlpha = Mathf.Lerp (m_colliderRenderer.material.color.a, 0.0f, Time.fixedDeltaTime * lerpVelocity * 3.5f); 
+				m_colliderRenderer.material.color = new Color (m_colliderRenderer.material.color.r, m_colliderRenderer.material.color.g, m_colliderRenderer.material.color.b, _lerpedAlpha); 
+				m_trueDistance = Mathf.Lerp (m_trueDistance, _hit.distance, Time.fixedDeltaTime * lerpVelocity * 2f);
+				m_obstacleRaycastEntered = true; 
 				Debug.Log (-m_trueDistance); 
-			} else if((_hit.collider.gameObject.layer ==LayerMask.NameToLayer(fadeleLayerString))){
-				m_alphaFadeRenderer = _hit.collider.GetComponent<MeshRenderer> (); 
-				_hitMaterial = m_alphaFadeRenderer.material; 
-				_hitMaterial.color = new Color (_hitMaterial.color.r, _hitMaterial.color.g, _hitMaterial.color.b, 0.5f); 
-				m_alphaFadeRenderer.material = _hitMaterial; 
-				m_alphaChanged = true; 
+
+
+			} else if ((_hit.collider.gameObject.layer == LayerMask.NameToLayer (fadeleLayerString))) {
+				m_colliderRenderer = _hit.collider.GetComponent<MeshRenderer> (); 
+				//_hitMaterial = m_colliderRenderer.material; 
+				float _lerpedAlpha = Mathf.Lerp (m_colliderRenderer.material.color.a, 0.5f, Time.fixedDeltaTime * lerpVelocity * 3f); 
+				m_colliderRenderer.material.color = new Color (m_colliderRenderer.material.color.r, m_colliderRenderer.material.color.g, m_colliderRenderer.material.color.b, _lerpedAlpha); 
+				//m_colliderRenderer.material = _hitMaterial; 
+				m_fadeRaycastEntered = true; 
 			}
 
+			//Why Not working?
 //			switch (_hit.collider.gameObject.layer) {
 //			case LayerMask.NameToLayer(obstacleLayerString):
 //				m_trueDistance = Mathf.Lerp (m_trueDistance, _hit.distance, Time.fixedDeltaTime * lerpVelocity * 3f);
@@ -108,10 +119,23 @@ public class ThirdPersonCameraMovement : MonoBehaviour
 
 		} else {
 			m_trueDistance = Mathf.Lerp (m_trueDistance, distance, Time.fixedDeltaTime * lerpVelocity); 
-			if (m_alphaChanged && m_alphaFadeRenderer != null) {
-				m_alphaFadeRenderer.material.color = new Color (m_alphaFadeRenderer.material.color.r, m_alphaFadeRenderer.material.color.g, m_alphaFadeRenderer.material.color.b, 1f); 
-				m_alphaFadeRenderer = null; 
-				m_alphaChanged = false; 
+			if (m_fadeRaycastEntered && m_colliderRenderer != null) {
+				float _lerpedAlpha = Mathf.Lerp (m_colliderRenderer.material.color.a, 1f, Time.fixedDeltaTime * lerpVelocity); 
+				m_colliderRenderer.material.color = new Color (m_colliderRenderer.material.color.r, m_colliderRenderer.material.color.g, m_colliderRenderer.material.color.b, _lerpedAlpha); 
+				if (_lerpedAlpha >= 0.9f) {
+					m_colliderRenderer.material.color = new Color (m_colliderRenderer.material.color.r, m_colliderRenderer.material.color.g, m_colliderRenderer.material.color.b, 1f); 
+					m_colliderRenderer = null; 
+					m_fadeRaycastEntered = false; 
+				}
+			} else if (m_obstacleRaycastEntered && m_colliderRenderer != null) {
+				//m_colliderRenderer.enabled = true; 
+				float _lerpedAlpha = Mathf.Lerp (m_colliderRenderer.material.color.a, 1.0f, Time.fixedDeltaTime * lerpVelocity); 
+				m_colliderRenderer.material.color = new Color (m_colliderRenderer.material.color.r, m_colliderRenderer.material.color.g, m_colliderRenderer.material.color.b, _lerpedAlpha); 
+				if (_lerpedAlpha >= 0.9f) {
+					m_colliderRenderer.material.color = new Color (m_colliderRenderer.material.color.r, m_colliderRenderer.material.color.g, m_colliderRenderer.material.color.b, 1f);
+					m_colliderRenderer = null; 
+					m_obstacleRaycastEntered = false; 
+				}
 			}
 
 		}
