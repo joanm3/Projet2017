75edf06a71b9cb9870faa7ab77ffe44b10961245
joanm3 <joanm3@hotmail.com>
2016-11-30 20:27:24 +0100

    [Surface] Added rotations tests in CameraTest1 scene

---
+++ Assets/ConstantRotation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantRotation : MonoBehaviour {
+
+    public enum AxisRotation { x, y, z };
+    public AxisRotation axisRotation = AxisRotation.x;
+    public float rotationAngleForce = 2f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        switch (axisRotation)
+        {
+            case AxisRotation.x:
+                transform.eulerAngles += new Vector3(rotationAngleForce * Time.deltaTime, 0f, 0f);
+                break;
+            case AxisRotation.y:
+                transform.eulerAngles += new Vector3(0f, rotationAngleForce * Time.deltaTime, 0f);
+                break;
+            case AxisRotation.z:
+                transform.eulerAngles += new Vector3(0f, 0f, rotationAngleForce * Time.deltaTime);
+                break;
+
+
+        }
+
+    }
+}
---
+++ Assets/RotateSinus.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateSinus : MonoBehaviour {
+
+
+    public enum AxisRotation { x, y, z};
+
+    public AxisRotation axisRotation = AxisRotation.z;
+
+    public float forceAngle = 70f;
+
+    public float velocityNotConstant = 0.5f; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+
+        switch(axisRotation)
+        {
+            case AxisRotation.x:
+                transform.rotation = Quaternion.Euler(Mathf.Sin(Time.realtimeSinceStartup * velocityNotConstant) * forceAngle, 0f, 0f);
+                break;
+            case AxisRotation.y:
+                transform.rotation = Quaternion.Euler(0, Mathf.Sin(Time.realtimeSinceStartup * velocityNotConstant) * forceAngle, 0f);
+                break;
+            case AxisRotation.z:
+                transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.realtimeSinceStartup * velocityNotConstant) * forceAngle);
+                break;
+
+
+        }
+
+    }
+}
